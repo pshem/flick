6a9924a96e9e0705f38ee14bda46c4310a8237dc
pshem <ppikula@rocketmail.com>
2018-02-08 18:27:09 +0000

    Made the code more robust against spelling errors
    Switched to using a cases statement, started catching invalid device names

--- flick.sh
+++ flick.sh
@@ -1,26 +1,23 @@
 #! /bin/bash
 
+#Copyright @pshem & @ZsanettM 2017-2018, LGPL-3.0
+
 #strict mode
 set -euo pipefail
 
 #Check if an argument has been passed to the script
-if [ $# -eq 1 ];
-then
-	deviceName=$1
-elif [ $# -eq 0 ]; #with no argumnets, default to Touchscreen
-then
-	deviceName=Touchscreen
-else
-	echo "Invalid number of arguments. Flick expects up to 1 device name. If your device name contains a space, quote it"
-	exit 1;
-fi
-echo $deviceName
+case $# in #depending on the number of arguments
+0) deviceName=Touchscreen;;
+1) deviceName=$1;;
+*) echo "Invalid number of arguments. Flick expects up to 1 device name. If your device name contains a space, quote it"
+	exit 2;;
+esac
 
 #What is the number of the device?
-deviceNumber=$(xinput | grep -i "$deviceName" | awk 'BEGIN { FS = "=" } ; { print $2 }' | awk '{ print $1 }')
+deviceNumber=$(xinput | grep -i "$deviceName" | awk 'BEGIN { FS = "=" } ; { print $2 }' | awk '{ print $1 }') || (echo "Invalid device"; exit 3)
 
 #Check device status
-if [[ $(xinput list-props "$deviceNumber" |  grep -i 'Device Enabled' |  awk '{print $4}') == '1' ]]
+if [[ $(xinput list-props "$deviceNumber" |  grep -i 'Device Enabled' |  awk '{print $4}') -eq 1 ]]
 #if enabled, disable
 then
 	xinput disable "$deviceNumber"
